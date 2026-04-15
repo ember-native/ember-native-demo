4ff1260588d5fcf6901b21428919786f9565368a
Patrick Pircher <patrick.pircher@ibm.com>
2026-04-15 17:09:24 +0200

    update

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -4,6 +4,9 @@ export default {
   id: 'org.nativescript.embernativedemo',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
+  bundler: 'vite',
+  bundlerConfigPath: 'vite.config.ts',
+  version: '1.0.0',
   android: {
     v8Flags: '--expose_gc',
     markingMode: 'none'
