afe55cd4792cd3ab04905c12fabed60925386ba1
Patrick Pircher <patricklx@users.noreply.github.com>
2026-05-02 11:11:57 +0200

    Add package manager configuration for CLI

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -4,6 +4,9 @@ export default {
   id: 'org.nativescript.embernativedemo',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
+  cli: {
+    packageManager: 'pnpm',
+  },
   android: {
     v8Flags: '--expose_gc',
     markingMode: 'none'
